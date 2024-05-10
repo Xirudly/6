4042ca424f597494cf8c571ecca5c541a94edebf
Xirudly <734428735@qq.com>
2024-05-10 16:28:13 +0800

    Revert charge temperature changes

--- drivers/power/supply/qcom/qpnp-fg-gen4.c
+++ drivers/power/supply/qcom/qpnp-fg-gen4.c
@@ -6230,7 +6230,7 @@ static void soc_work_fn(struct work_struct *work)
 		pr_err("Error in getting batt_temp, rc=%d\n", rc);
 
 
-	if (temp < 480 && fg->last_batt_temp >= 480) {
+	if (temp < 450 && fg->last_batt_temp >= 450) {
 		/* follow the way that fg_notifier_cb use wake lock */
 		pm_stay_awake(fg->dev);
 		schedule_work(&fg->status_change_work);
